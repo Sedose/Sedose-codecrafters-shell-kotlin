2f78e5450c08464d46d9933d8297991c38647fc0
Sedose <Sedose>
2025-07-24 23:05:37 +0300

    ;oawkdaw

--- src/main/kotlin/io/codecrafters/builtins/EchoBuiltin.kt
+++ src/main/kotlin/io/codecrafters/builtins/EchoBuiltin.kt
@@ -2,10 +2,13 @@ package io.codecrafters.builtins
 
 import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
+import org.springframework.shell.standard.ShellOption
 
 @ShellComponent
 class EchoBuiltin {
 
     @ShellMethod(key = ["echo"], value = "Echo the given arguments")
-    fun echo(line: String): String = line
+    fun echo(
+        @ShellOption(arity = Int.MAX_VALUE) words: Array<String>
+    ): String = words.joinToString(" ")
 }
