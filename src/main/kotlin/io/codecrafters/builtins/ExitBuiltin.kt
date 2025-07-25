22f4f735d4ff678513c88c9776556272dcccf764
Sedose <Sedose>
2025-07-25 21:32:10 +0300

    codecrafters submit [skip ci]

--- src/main/kotlin/io/codecrafters/builtins/ExitBuiltin.kt
+++ src/main/kotlin/io/codecrafters/builtins/ExitBuiltin.kt
@@ -1,17 +1,16 @@
 package io.codecrafters.builtins
 
+import org.springframework.shell.ExitRequest
 import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
 import org.springframework.shell.standard.ShellOption
-import org.springframework.shell.ExitRequest
+import org.springframework.shell.standard.commands.Quit
 
 @ShellComponent
-class ExitBuiltin {
+class ExitBuiltin : Quit.Command {
 
     @ShellMethod(key = ["exit", "quit"], value = "Terminate the shell process")
     fun exit(
         @ShellOption(defaultValue = "0") exitCode: Int,
-    ): Nothing {
-        throw ExitRequest(exitCode)
-    }
+    ): Nothing = throw ExitRequest(exitCode)   // ← clean Spring‑Shell shutdown
 }
