760f9c4efdcb07f04b59fb0270c1031f0a959541
Sedose <Sedose>
2025-08-03 09:08:34 +0300

    codecrafters submit [skip ci]

--- src/main/kotlin/io/codecrafters/Config.kt
+++ src/main/kotlin/io/codecrafters/Config.kt
@@ -1,11 +1,6 @@
 package io.codecrafters
 
 import io.codecrafters.command.BuiltinCommandHandler
-import org.jline.reader.LineReader
-import org.jline.reader.LineReaderBuilder
-import org.jline.reader.impl.DefaultParser
-import org.jline.terminal.Terminal
-import org.jline.terminal.TerminalBuilder
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -23,24 +18,4 @@ class Config {
         ExitExecutor { status ->
             exitProcess(status)
         }
-
-    @Bean
-    fun terminal(): Terminal =
-        try {
-            TerminalBuilder.builder()
-                .system(true)
-                .build()
-        } catch (e: Exception) {
-            TerminalBuilder.builder()
-                .system(false)
-                .dumb(true)
-                .build()
-        }
-
-    @Bean
-    fun lineReader(terminal: Terminal): LineReader =
-        LineReaderBuilder.builder()
-            .terminal(terminal)
-            .parser(DefaultParser())
-            .build()
 }
